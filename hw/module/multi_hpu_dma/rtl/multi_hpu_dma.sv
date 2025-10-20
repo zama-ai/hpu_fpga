39760a9c13726ed0d053f18c1ee46930afff71d3
lsainati <lucas.sainati@zama.ai>
2025-10-20 11:29:01 +0200

    [CLEANUP] deleting mux and first cleanup

--- hw/module/multi_hpu_dma/rtl/multi_hpu_dma.sv
+++ hw/module/multi_hpu_dma/rtl/multi_hpu_dma.sv
@@ -317,31 +317,22 @@ module multi_hpu_dma
   // depending on line_sel signal, selects and outputs the correct line
   // this module is fully combinatory
   // ============================================================================================ //
-  mhdma_axis_selector # (
-  ) mhdma_axis_selector (
-    .qsfp_rx_tdata      (qsfp_rx_tdata),
-    .qsfp_rx_tkeep_user (qsfp_rx_tkeep_user),
-    .qsfp_rx_tlast      (qsfp_rx_tlast),
-    .qsfp_rx_tvalid     (qsfp_rx_tvalid),
+  // Rx Link
+  assign axis_rx_tdata      = qsfp_rx_tdata[line_sel];
+  assign axis_rx_tkeep_user = qsfp_rx_tkeep_user[line_sel];
+  assign axis_rx_tlast      = qsfp_rx_tlast[line_sel];
+  assign axis_rx_tvalid     = qsfp_rx_tvalid[line_sel];
 
-    .qsfp_tx_tdata      (qsfp_tx_tdata),
-    .qsfp_tx_tkeep_user (qsfp_tx_tkeep_user),
-    .qsfp_tx_tlast      (qsfp_tx_tlast),
-    .qsfp_tx_tvalid     (qsfp_tx_tvalid),
-    .qsfp_tx_tready     (qsfp_tx_tready),
+  // TX link
+  assign axis_tx_tready = qsfp_tx_tready[line_sel];
 
-    .axis_rx_tdata      (axis_rx_tdata),
-    .axis_rx_tkeep_user (axis_rx_tkeep_user),
-    .axis_rx_tlast      (axis_rx_tlast),
-    .axis_rx_tvalid     (axis_rx_tvalid),
-
-    .axis_tx_tdata      (axis_tx_tdata),
-    .axis_tx_tkeep_user (axis_tx_tkeep_user),
-    .axis_tx_tlast      (axis_tx_tlast),
-    .axis_tx_tvalid     (axis_tx_tvalid),
-    .axis_tx_tready     (axis_tx_tready),
-
-    .line_sel(line_sel)
-  );
+  generate
+    for (genvar i = 0; i < QSFP_LANE_NB; i++) begin
+      assign qsfp_tx_tdata[i]       = (line_sel == i) ? axis_tx_tdata      : 'h0;
+      assign qsfp_tx_tkeep_user[i]  = (line_sel == i) ? axis_tx_tkeep_user : 'h0;
+      assign qsfp_tx_tlast[i]       = (line_sel == i) ? axis_tx_tlast      : 'h0;
+      assign qsfp_tx_tvalid[i]      = (line_sel == i) ? axis_tx_tvalid     : 'h0;
+    end
+  endgenerate
 
 endmodule
