b4506c1dfd4420b53d851517ac84d4bf4a665d0f
Baptiste Roux <baptiste.roux@zama.ai>
2026-03-19 22:08:28 +0100

    [TRACE] Rework isc trace data-alignement
    Aims is to have a simple pasing on SW side
    
    Also fix issue with refill value and timestamp.
    Previously trace word issue from refill contains invalid instruction
    instead of the refilled one.

--- hw/module/instruction_scheduler/module/isc_common/rtl/isc_common_param_pkg.sv
+++ hw/module/instruction_scheduler/module/isc_common/rtl/isc_common_param_pkg.sv
@@ -33,17 +33,20 @@ package isc_common_param_pkg;
   localparam int ISC_INFO_W = $bits(isc_info_t);
 
 //= Trace
+// NB: Trace is build to be parsed by a CPU, thus we force byte align fields
   localparam int TIMESTAMP_W = 32;
-  // Currently only pep is using this field
-  localparam int TRACE_RESV_W = LWE_K_W;
 
   // TraceInfo structure
   typedef struct packed {
-    logic[TIMESTAMP_W-1: 0] timestamp;
-    logic [PE_INST_W-1: 0]  insn;
-    isc_query_cmd_e         cmd;
-    isc_pool_state_t        state;
-    logic[TRACE_RESV_W-1:0] pe_reserved;
+    logic [TIMESTAMP_W-1: 0] timestamp;
+    logic [31: 0] insn;
+    logic [7: 0] sync_id;
+    logic [7: 0] issue_lock;
+    logic [7: 0] rd_lock;
+    logic [7: 0] wr_lock;
+    logic [7: 0] cmd;
+    logic [7: 0] vld_rdpdg_pdg;
+    logic [15: 0] pe_reserved;
   } isc_trace_t;
 localparam int TRACE_W = $bits(isc_trace_t);
 
