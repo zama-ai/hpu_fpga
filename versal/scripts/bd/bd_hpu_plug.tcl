9648c846c36ab2c405564d5b4c2072842b220e43
lsainati <lucas.sainati@zama.ai>
2026-04-14 11:59:17 +0200

    [FIX] reverting HPU_CT_HBM BW to 12000

--- versal/scripts/bd/bd_hpu_plug.tcl
+++ versal/scripts/bd/bd_hpu_plug.tcl
@@ -168,8 +168,8 @@ namespace eval _nsp_hpu {
     variable HPU_KSK_HBM_DATA_W 256
 
     # CT <-> HBM
-    variable HPU_CT_HBM_RD_BW 4000
-    variable HPU_CT_HBM_WR_BW 4000
+    variable HPU_CT_HBM_RD_BW 12000
+    variable HPU_CT_HBM_WR_BW 12000
     variable HPU_CT_HBM_RD_BURST_AVG 16
     variable HPU_CT_HBM_WR_BURST_AVG 16
     variable HPU_CT_HBM_BURST_MAX 16
